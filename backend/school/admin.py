0c2263bb79f6b8256d2f3f93c0642ee003104847
Your Name <you@example.com>
2025-10-07 19:00:17 +0300

    Update school admin configuration

--- backend/school/admin.py
+++ backend/school/admin.py
@@ -201,8 +201,6 @@ class UserAdmin(DjangoUserAdmin):
     list_display = (
         "username",
         "email",
-        "first_name",
-        "last_name",
         "is_active",
         "is_staff",
         "is_superuser",
